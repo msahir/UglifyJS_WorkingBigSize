3215acb5c24e319b29a472c6757459cf95a23988
MAS <mohitaheer@gmail.com>
2015-05-06 16:51:26 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/til/App.java
@@ -0,0 +1,41 @@
+package com.til;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger logger = LoggerFactory
+			.getLogger(App.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String content = null;
+		try {
+			content = "  function func(a,b){  "+
+					 "   return a * b \n }   ";
+			//logger.error(minify(content));
+			logger.info(minify((content)));
+		} catch (Exception ex) {
+			logger.error(ex.getStackTrace() + ex.getMessage());
+		}
+	}
+
+	public static String minify(String content) {
+		String compressedCode = null;
+		try {
+			UglifyJSUtil uglifyJSUtil = new UglifyJSUtil();
+			compressedCode = uglifyJSUtil.processJS(content);
+		} catch (Exception ex) {
+			logger.debug(ex.getStackTrace() + ex.getMessage());
+		}
+		return compressedCode;
+	}
+}
